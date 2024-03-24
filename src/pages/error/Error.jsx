a6398fff5deed8c0e6a44ed98665ec04d3ad544d
roushan-sheik <roushansheik@gmail.com>
2024-03-24 12:09:55 +0600

    react router dom error page added

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,37 @@
+import { Button } from "@material-tailwind/react";
 import React from "react";
-
+import { Link, useRouteError } from "react-router-dom";
 const Error = () => {
+  const error = useRouteError();
   return (
-    <div>
-      <h2>404 Page Not Found</h2>
+    <div className="flex flex-col gap-6 items-center mt-[100px] h-screen">
+      <h2 className="font-bold text-4xl ">404 Page Not Found</h2>
+      <div>
+        <h1 className="font-bold text-2xl ">Oops!</h1>
+        <p className=" text-red-500">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Button variant="outlined" className="flex mt-6 items-center gap-3">
+          <Link to={"/"}>Got Home</Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+        </Button>
+      </div>
     </div>
   );
 };
